57ec5309f193bff98d4ae753779ad7861d13a33f
albgarre <garre.alberto@gmail.com>
2022-04-08 12:18:10 +0200

    Adapted time_to_size for the new classes

--- R/functions_counts.R
+++ R/functions_counts.R
@@ -180,33 +180,170 @@ distribution_to_logcount <- function(model, log_count) {
 #' or [MCMCgrowth]
 #' @param size Target population size (in log scale)
 #' 
+#' @importFrom purrr map_dbl
+#' 
 #' @return If "model" is [IsothermalGrowth] or [DynamicGrowth], a number. If "model"
 #' is [StochasticGrowth] or [MCMCgrowth], an instance of [TimeDistribution]
 #' 
 #' @export
 #' 
-time_to_size <- function(model, size, 
-                         # type = "discrete", 
-                         logbase = c("natural", "10")  # TODO
+#' @examples 
+#' 
+#' ## Example 1 - Growth predictions -------------------------------------------
+#' 
+#' ## The model is defined as usual with predict_growth
+#' 
+#' my_model <- list(model = "modGompertz", logN0 = 0, C = 6, mu = .2, lambda = 20)
+#' 
+#' my_time <- seq(0, 100, length = 1000)  # Vector of time points for the calculations
+#' 
+#' my_prediction <- predict_growth(my_time, my_model, environment = "constant")
+#' 
+#' plot(my_prediction)
+#' 
+#' ## We just have to pass the model and the size (in log10)
+#' 
+#' time_to_size(my_prediction, 3)
+#' 
+#' ## If the size is not reached, it returns NA
+#' 
+#' time_to_size(my_prediction, 8)
+#' 
+#' ## Example 2 - Model fit ----------------------------------------------------
+#' 
+#' my_data <- data.frame(time = c(0, 25, 50, 75, 100), 
+#'                       logN = c(2, 2.5, 7, 8, 8))
+#'                       
+#' models <- list(primary = "Baranyi")
+#' 
+#' known <- c(mu = .2)
+#' 
+#' start <- c(logNmax = 8, lambda = 25, logN0 = 2)
+#' 
+#' primary_fit <- fit_growth(my_data, models, start, known,
+#'                           environment = "constant",
+#'                           )
+#'                           
+#' plot(primary_fit)
+#' 
+#' time_to_size(primary_fit, 4)
+#' 
+#' ## Example 3 - Global fitting -----------------------------------------------
+#' 
+#' ## We need a model first
+#' 
+#' data("multiple_counts")
+#' data("multiple_conditions")
+#' 
+#' sec_models <- list(temperature = "CPM", pH = "CPM")
+#' 
+#' known_pars <- list(Nmax = 1e8, N0 = 1e0, Q0 = 1e-3,
+#'                    temperature_n = 2, temperature_xmin = 20, 
+#'                    temperature_xmax = 35,
+#'                    pH_n = 2, pH_xmin = 5.5, pH_xmax = 7.5, pH_xopt = 6.5)
+#'                    
+#' my_start <- list(mu_opt = .8, temperature_xopt = 30)
+#' 
+#' global_fit <- fit_growth(multiple_counts, 
+#'                          sec_models, 
+#'                          my_start, 
+#'                          known_pars,
+#'                          environment = "dynamic",
+#'                          algorithm = "regression",
+#'                          approach = "global",
+#'                          env_conditions = multiple_conditions
+#'                          ) 
+#'                          
+#' plot(global_fit)
+#' 
+#' ## The function calculates the time for each experiment
+#' 
+#' time_to_size(global_fit, 3)
+#' 
+#' ## It returns NA for the particular experiment if the size is not reached
+#' 
+#' time_to_size(global_fit, 4.5)
+#' 
+time_to_size <- function(model, 
+                         size, 
+                         type = "discrete"
                          ) {
     
-    ## Just a top level function that calls the superseded functions
-    
-    my_class <- class(model)
-    
-    if ( my_class %in% c("IsothermalGrowth", "DynamicGrowth") ) {
+    if (type == "discrete") {  # Calculation of a discrete time ----------------
         
-        time_to_logcount(model, size)
+        if (is.GrowthPrediction(model)) {  # For model predictions
+            
+            d <- model$simulation
+            
+            approx(x = d$logN, y = d$time, size, 
+                   ties = function(x) min(x, na.rm = TRUE))$y
+            
+        } else if (is.GrowthFit(model)) {  # For model fits using the single approach
+            
+            d <- model$best_prediction$simulation
+            
+            approx(x = d$logN, y = d$time, size, 
+                   ties = function(x) min(x, na.rm = TRUE))$y
+            
+        } else if (is.GlobalGrowthFit(model)) {  # For global fits
+
+            model$best_prediction %>%
+                map_dbl(~ approx(x = .$simulation$logN, 
+                                 y = .$simulation$time, 
+                                 size, 
+                                 ties = function(x) min(x, na.rm = TRUE))$y
+                        )
+            
+        } else if (is.GrowthUncertainty(model)) {  # For growth with uncertainty
+            
+            d <- model$quantiles
+            
+            approx(x = d$q50, y = d$time, size,  # Interpolate the median
+                   ties = function(x) min(x, na.rm = TRUE))$y
+            
+        } else if (is.MCMCgrowth(model)) {  # TODO: update this?
+            
+        } else {
+            stop("Model type not supported for discrete calculations: ", class(model))
+        }
         
-    } else if ( my_class %in% c("StochasticGrowth", "MCMCgrowth") ) {
+    } else if (type == "distribution") {  # Calculation of the distribution ----
         
-        distribution_to_logcount(model, size)
+        if (is.GrowthUncertainty(model)) {
+            
+            time_dist <- split(model$simulations, model$simulations$iter) %>%
+                map_dfr(~ approx(.$logN, .$time, size, 
+                                 ties = function(x) min(x, na.rm = TRUE))
+                )
+            
+        } else if (is.MCMCgrowth(model)) {  # TODO
+            
+        } else {
+            stop("Model type not supported for calculating distributions: ", class(model))
+        }
+        
+        ## Put the calculations together and return
+        
+        my_summary <- time_dist %>%
+            summarize(m_time = mean(.data$y, na.rm = TRUE),
+                      sd_time = sd(.data$y, na.rm=TRUE),
+                      med_time = median(.data$y, na.rm = TRUE),
+                      q10 = quantile(.data$y, .1, na.rm = TRUE),
+                      q90 = quantile(.data$y, .9, na.rm = TRUE))
+        
+        out <- list(distribution = time_dist$y,
+                    summary = my_summary)
+        
+        class(out) <- c("TimeDistribution", class(out))
+        
+        out
         
     } else {
         
-        stop("Class not supported: ", my_class)
+        stop("type must be 'discrete' or 'distribution', not ", type)
         
     }
+
     
 }
 
